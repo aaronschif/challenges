3f17e40af30a48a887cf3887e07171db48faaca4
Aaron Schif <aschif@netdevgroup.com>
2016-06-21 12:46:23 -0500

    Cleaner implementation

--- challenges/surf/main.rs
+++ challenges/surf/main.rs
@@ -5,42 +5,31 @@ use std::fmt;
 
 struct Wave {
     start: u32,
-    fun: u32,
     wait: u32,
+    fun: u32,
+    max_fun_do: u32,
+    max_fun_dont: u32,
 }
 
 impl Wave {
+    fn new(start: u32, fun: u32, wait: u32) -> Wave {
+        Wave {
+            start: start,
+            wait: wait,
+            fun: fun,
+            max_fun_do: fun,
+            max_fun_dont: fun,
+        }
+    }
+
     fn end(&self) -> u32 {
         self.start + self.wait
     }
-
-    fn overlap(&self, wave: &Wave) -> bool {
-        (self.start < wave.start && wave.start < self.end()) || (wave.start < self.start && self.start < self.end())
-    }
 }
 
-struct WaveData {
-    max_fun: u32,
-    next_wave: Option<u32>,
-}
-
-impl WaveData {
-    fn new(max_fun: u32) -> WaveData {
-        WaveData {
-            max_fun: max_fun,
-            next_wave: None,
-        }
-    }
-}
-
-impl fmt::Debug for WaveData {
+impl fmt::Debug for Wave {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if let Some(n) = self.next_wave {
-            write!(f, "WaveData {{ max_fun: {}, next_wave: {} }}", self.max_fun, n)
-        }
-        else {
-            write!(f, "WaveData {{ max_fun: {}, next_wave: {} }}", self.max_fun, "None")
-        }
+        write!(f, "Wave {{ start: {}, max_fun: {}, otherfun: {} }}", self.start, self.max_fun_do, self.max_fun_dont)
     }
 }
 
@@ -50,11 +39,11 @@ fn readall() -> Vec<Wave> {
 
     for line in stdin.lock().lines().map(|x| String::from(x.unwrap())).skip(1) {
         let mut parts = line.split_whitespace().map(|e| e.trim().parse::<u32>().unwrap());
-        let wave = Wave {
-            start: parts.next().unwrap(),
-            fun: parts.next().unwrap(),
-            wait: parts.next().unwrap(),
-        };
+        let wave = Wave::new(
+            parts.next().unwrap(),
+            parts.next().unwrap(),
+            parts.next().unwrap(),
+        );
         result.push(wave);
     }
     return result;
@@ -62,31 +51,23 @@ fn readall() -> Vec<Wave> {
 
 fn main() {
     let mut waves = readall();
-    waves.sort_by_key(|e| e.start);
-    let mut waves_values: Vec<WaveData> = Vec::new();
+    waves.sort_by_key(|e| e.end());
+
     let mut total_max_fun = 0;
 
-    for (n, wave) in waves.iter().enumerate() {
-        let mut max_fun = wave.fun;
-        let mut max_fun_wave = None;
+    for n in 0..waves.len() {
         for i in (0..n).rev() {
-            if waves[i].overlap(wave) {
-                continue;
-            }
-            else {
-                let new_fun = waves_values[i].max_fun + wave.fun;
-                if new_fun > max_fun {
-                    max_fun = new_fun;
-                    max_fun_wave = Some(i as u32);
-                }
+            if waves[i].end() <= waves[n].start {
+                waves[n].max_fun_do = max(waves[n].max_fun_do, waves[i].max_fun_dont + waves[n].fun);
+                break;
             }
         }
-        total_max_fun = max(total_max_fun, max_fun);
-        let mut wave_data = WaveData {
-            max_fun: max_fun,
-            next_wave: max_fun_wave,
-        };
-        waves_values.push(wave_data);
+
+        total_max_fun = max(total_max_fun, waves[n].max_fun_dont);
+        total_max_fun = max(total_max_fun, waves[n].max_fun_do);
+        waves[n].max_fun_dont = total_max_fun;
+        // println!("{:?}", waves);
     }
     println!("{}", total_max_fun);
+    // println!("{}", waves.last().map_or(0, |x| x.max_fun_dont));
 }
--- challenges/surf/run.py
+++ challenges/surf/run.py
@@ -8,39 +8,50 @@ from time import time
 parser = argparse.ArgumentParser()
 parser.add_argument('cmd')
 parser.add_argument('path')
-parser.add_argument('--timeout', nargs='?', default=5, type=int)
+parser.add_argument('--timeout', nargs='?', default=5, type=float)
 
 args = parser.parse_args()
+
+def run_test(cmd, stdin, timeout):
+    proc = Popen(cmd, stdin=PIPE, stdout=PIPE)
+    try:
+        outs, errs = proc.communicate(stdin, timeout=timeout)
+        goal = test_path.read_bytes()
+    except TimeoutExpired:
+        proc.terminate()
+        outs = None
+    return outs
 
 try:
     for path in Path(args.path).glob('*.in'):
         test_path = path.with_suffix('.out')
         assert test_path.exists()
 
-        total_time = -time()
-        proc = Popen(args.cmd, stdin=PIPE, stdout=PIPE)
-        try:
-            outs, errs = proc.communicate(path.read_bytes(), timeout=args.timeout)
-        except TimeoutExpired:
-            proc.terminate()
-            outs = None
+        stdin_size = path.stat().st_size//1024
 
-        correct = outs == test_path.read_bytes()
+        total_time = -time()
+        if stdin_size <= 50000:
+            our_output = run_test(args.cmd, path.read_bytes(), args.timeout)
+            correct_output = test_path.read_bytes()
+            correctness = our_output == correct_output
+        else:
+            correctness = None
 
         total_time += time()
-        reason = ""
 
-        if correct:
-            pass
+        if correctness:
+            reason = ""
+        elif correctness is None:
+            reason = "skipped"
         elif total_time > args.timeout:
             reason = "timeout"
-        elif proc.returncode is not 0:
+        elif 0 is not 0:
             reason = "returncode"
         else:
-            reason = "diff"
+            reason = "diff ({} != {})".format(our_output, correct_output)
 
 
-        print("{} {:8} {:>20} [{:6}k]   {:6.2f}s".format("\033[92m✓\033[0m" if correct else "\033[91m✗\033[0m", reason, path.name, path.stat().st_size//1024, total_time))
+        print("{} {:8} {:>20} [{:6}k]   {:6.2f}s".format("\033[92m✓\033[0m" if correctness else "\033[91m✗\033[0m", reason, path.name, stdin_size, total_time))
         # if not correct and outs:
 except KeyboardInterrupt:
     pass
